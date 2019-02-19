9ceae3fb09a1164058115f72bf0e786a53c71698
Angela Kearns <angela0527.k@gmail.com>
2019-02-19 12:27:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-/*
-This is a JavaScript (JS) file.
-JavaScript is the programming language that powers the web.
-
-To use this file, place the following <script> tag just before the closing </body> tag in your HTML file, making sure that the filename after "src" matches the name of your file...
-
-    <script src="script.js"></script>
-
-Learn more about JavaScript at https://developer.mozilla.org/en-US/Learn/JavaScript
-
-When you're done, you can delete all of this grey text, it's just a comment.
-*/
-
+//getHelp displays a textbox 
 function getHelp() {
 	alert("To begin, input the values for the height and width of the two matrices\n" + 
 	" *Remember the width of the first matrix must be equal to the height of the second matrix*\n"
@@ -23,6 +11,7 @@ function checkInput() {
   var h2 = document.forms["form1"]["h2"].value;
   var w2 = document.forms["form1"]["w2"].value;
   var compH2 = h2;
+  //if the width of the first is not equal to the height of the second the matrices cannot be multiplied
   if (w1 != compH2){
     alert("First matrix width must equal second matrix height");
     return false;
@@ -32,7 +21,9 @@ function checkInput() {
   var j=1;
   while (j<=h1){
   	while (i<=w1){
-    	var id = "m" + j + i;
+    	//create inputs for the values in the matrix i describes the height and j second the width
+	//formats them into an i by j matrix format
+	var id = "m" + j + i;
   		var matr = document.createElement("input");
   		matr.id = id;
   		matr.size = "4";
